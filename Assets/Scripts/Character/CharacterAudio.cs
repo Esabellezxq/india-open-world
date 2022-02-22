2498cc485632920cb40927e6dabb892e7fa81cf5
Esabellezxq <562401579@qq.com>
2022-02-22 16:19:36 +0800

    feat: 添加人物脚步声

---
+++ Assets/Scripts/Character/CharacterAudio.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAudio : MonoBehaviour
+{
+  public AudioSource audioSource;
+  public AudioClip audioClip;
+  void Start()
+  {
+
+  }
+
+  public void PlayFootStepAudio()
+  {
+    Debug.Log("PlayFootStepAudio");
+    audioSource.PlayOneShot(audioClip);
+    audioSource.Play();
+  }
+}
